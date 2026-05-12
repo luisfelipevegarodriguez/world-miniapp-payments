384ac168c1cff731e68947f98cd5fd8055722db8
luisfelipevegarodriguez <luisfelipevegarodriguez@gmail.com>
2026-05-12 13:12:43 +0200

    feat: ELITE_2026_PROTOCOL – god-mode-ci + autofix.ci + ESLint Crockford

---
+++ eslint.config.js
@@ -0,0 +1,12 @@
+export default [
+  {
+    rules: {
+      "eqeqeq": ["error", "always"],
+      "no-plusplus": "error",
+      "no-eval": "error",
+      "no-var": "error",
+      "prefer-const": "error",
+      "curly": ["error", "all"]
+    }
+  }
+];
